9d88b6f693e78ad8a20d28219797751f7a1a9a69
raiyan-codes <gr2257@nyu.edu>
2025-05-07 17:31:36 -0400

    v6

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
 	content: [
 		"./app/**/*.{js,ts,jsx,tsx}",
+		"./src/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
 		"./mdx-components.tsx",
 		"content/**/*.mdx",
 	],
